2cc740c30adc687a9f8de2436fc7774baed5a49b
Enzu <100221308+Enzujp@users.noreply.github.com>
2023-07-24 16:02:49 +0100

    question 6-1

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+// question 6-1 Solution
+function isPalindrome(string) {
+    // find the length of the string. 
+    const len = string.length;
+    // run a for loop through the first half of string input
+    for (let i=0; i=len/2; i++) {
+        //compare first half of string with last half
+        if (string[i] !== string[len - 1 - i]) {
+            reverseString(string);
+        } 
+            else if (string.length < 2){
+                console.log("enter a string that is at least two digits")
+            } else (console.log("It's a palindrome "))
+        }
+}
+
+function reverseString(string) {
+    // using the split(),reverse(), and join() builtin functions 
+    const reverse  = string.split("").reverse().join("");
+    console.log(reverse);
+
+}
+
 
