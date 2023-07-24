5744dd01d3b787d65b527e8cfe4251b09adf402e
Enzu <100221308+Enzujp@users.noreply.github.com>
2023-07-24 18:04:43 +0100

    Update q6-2.js

--- q6-2.js
+++ q6-2.js
@@ -1,20 +1,20 @@
-function findMissingNumber(arr){
+function findMissingNumber(collection){
     //create a set to store the range of numbers
     const set = new Set();
     // loop through set and populate with range of contents in array
-    for (let i=0; i < arr.length; i++);
-    set.add(arr[i]);
+    for (let i=0; i < collection.length; i++);
+    set.add(collection[i]);
 }
 
 //loop through array containing missing figures and compare values to set contents 
 //containing complete numbers and identify missing number(s)
 
-    for (let i = 1; i <= arr.length + 1; i++) {
+    for (let i = 1; i <= collection.length + 1; i++) {
         if (!set.has(i)) {
             return i;
         }
     }
 
-var arr = [1, 2, 3, 4, 5, 7, 8, 9];
+var testarray = [1, 2, 3, 4, 5, 7, 8, 9];
 
-findMissingNumber(arr);
+findMissingNumber(testarray);
