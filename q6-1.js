0f6b53b0a2a13a0044411d006930c0e78840e3ac
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-07-24 19:21:03 +0100

    restructuring q6-1

--- q6-1.js
+++ q6-1.js
@@ -1,25 +1,11 @@
 // question 6-1 Solution
-function isPalindrome(string) {
-    // find the length of the string. 
-    const len = string.length;
-    // run a for loop through the first half of string input
-    for (let i=0; i=len/2; i++) {
-        //compare first half of string with last half
-        if (string[i] !== string[len - 1 - i]) {
-            reverseString(string);
-            break;
-        } 
-    }
-    console.log( "it's a palindrome");
+function isPalindrome(string){
+    //reverse string and store in a variable
+    const reverseString = string.split("").reverse().join("");
+    //compare value of reversed string with normal string value to check if they are the same.
+    // print reverse value if they aren't and log message declaring palindrome if it is!
+    if (reverseString === string) {
+        console.log("It's a palindrome!");
+    } else (console.log(reverseString));
 }
-
-function reverseString(string) {
-    // using the split(),reverse(), and join() builtin functions 
-    const reverse  = string.split("").reverse().join("");
-    console.log(reverse);
-
-}
-
-// const userInput = prompt('Enter a string to check : ');
-
-isPalindrome('money')
+isPalindrome('adam');
